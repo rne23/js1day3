6de3e5aa7daa5a9263661295a3de18ae634dd9cb
rne23 <rne23@cornell.edu>
2017-10-01 16:13:41 -0700

    SECOND VERSION

--- js/app.js
+++ js/app.js
@@ -1,45 +1,28 @@
-name = prompt('Please enter your name:');
+askForNameAndColor();
 
-color = prompt (`Please enter your favorite color. Possible options:
-red, orange, yellow, green, blue, pink, purple, black, white, or grey.
-Uppercase or lowercase are acceptable. No punctuation marks.`);
+function askForNameAndColor(){
+  var name = prompt('Please enter your name:');
+  var color = prompt (`Please enter your favorite color. Possible options:
+  red, orange, yellow, green, blue, pink, purple, black, white, or grey.
+  Uppercase or lowercase are acceptable. No punctuation marks.`);
+  color = color.toLowerCase();
+  printNameAndColor(name, color);
+}
 
-color = color.toLowerCase();
-
-switch (color) {
-  case 'red':
-    alert('Your name is ' + name + ' and your favorite color is red.');
-    break;
-  case 'orange':
-    alert('Your name is ' + name + ' and your favorite color is orange.');
-    break;
-  case 'yellow':
-    alert('Your name is ' + name + ' and your favorite color is yellow.');
-    break;
-  case 'green':
-    alert('Your name is ' + name + ' and your favorite color is green.');
-    break;
-  case 'blue':
-    alert('Your name is ' + name + ' and your favorite color is blue.');
-    break;
-  case 'pink':
-    alert('Your name is ' + name + ' and your favorite color is pink.');
-    break;
-  case 'purple':
-    alert('Your name is ' + name + ' and your favorite color is purple.');
-    break;
-  case 'black':
-    alert('Your name is ' + name + ' and your favorite color is black.');
-    break;
-  case 'white':
-    alert('Your name is ' + name + ' and your favorite color is white.');
-    break;
-  case 'grey':
-    alert('Your name is ' + name + ' and your favorite color is grey.');
-    break;
-  default:
+function printNameAndColor(name, color) {
+  validColors = ['red', 'orange', 'yellow', 'green', 'blue', 'pink', 'purple',
+  'black', 'white', 'grey'];
+  if (validColors.includes(color) == true) {
+    var str = 'Your name is ' + name + ' and your favorite color is ' + color +
+    '.';
+    alert(str)
+    askForNameAndColor();
+  } else {
     alert('Your name is ' + name + `. However, you did not enter an acceptable
 color! Remember, the possible options are: red, orange, yellow, green, blue,
 pink, purple, black, white, or grey.`);
-    break;
+    var askAgain = confirm('Would you like to try again?');
+    if (askAgain == true) {
+      askForNameAndColor();}
+  }
 }
